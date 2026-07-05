a1b1e8eb952289a9bf5f225cd6d64a6f23ac0084
Ryan Wong <wongrt0502@gmail.com>
2026-07-05 23:04:52 +1000

    feat: yet another can driver refactor. Now supports multiple can peripherals by using a handle instead. No longer has state tied to the driver.c file (no more static vars)

--- test_code/isotp_test/Core/Inc/drivers/can_driver.h
+++ test_code/isotp_test/Core/Inc/drivers/can_driver.h
@@ -4,6 +4,9 @@
 #include "main.h"
 #include "drivers/errno.h"
 
+// =================================================================================================
+// Structs
+// =================================================================================================
 typedef struct {
     uint32_t fifo_overwrite;
     uint32_t tx_id_type;
@@ -13,52 +16,71 @@ typedef struct {
 } sr_fdcan_config_t;
 
 /**
+ * @brief Per-peripheral CAN driver state. One instance per FDCAN peripheral in use.
+ * Caller owns the storage and passes a pointer to every driver call, so multiple peripherals with independent
+ * configs can be used.
+ */
+typedef struct {
+    FDCAN_HandleTypeDef* hfdcan;
+    uint32_t tx_id_type;
+    uint32_t tx_brs;
+    uint32_t tx_frame_format;
+    uint32_t tx_event_fifo_control;
+    uint32_t filter_index;
+} sr_fdcan_handle_t;
+
+
+// =================================================================================================
+// Functions
+// =================================================================================================
+/**
  * @brief Configures the FD CAN peripheral
  * Handles configuration of things like the global filter, fifo overwrite etc.
- * Initiates tx header data which should be constant per CAN peripheral, stores in global var for now
+ * Initiates tx header data which should be constant per CAN peripheral, stores in the handle
  * Should also activate notification for relevant FIFOs if using interrupts
- * 
+ *
+ * @param handle - Driver state for this peripheral, filled in by this call
+ * @param hfdcan - HAL handle for the peripheral this handle will drive
  * @param config_struct - Struct as defined above
  * @return sr_errno_t - error code defined in errno.h
  */
-
-sr_errno_t sr_fdcan_config(FDCAN_HandleTypeDef* hfdcan, sr_fdcan_config_t* config_struct);
+sr_errno_t sr_fdcan_config(sr_fdcan_handle_t* handle, FDCAN_HandleTypeDef* hfdcan, sr_fdcan_config_t* config_struct);
 
 /**
- * @brief Adds a filter element 
+ * @brief Adds a filter element
  * If filter_type == range, then accepts IDs between id1 and id2
  * If filter_type == mask, id1 = filter and id2 = mask
  * If filter_type == dual, accept only id1 and id2 exactly
- * 
- * @param hfdcan 
+ *
+ * @param handle
  * @param filter_type - FDCAN_FILTER_RANGE or FDCAN_FILTER_MASK or FDCAN_FILTER_DUAL
  * @param filter_config - What to do with successful message, like FDCAN_FILTER_TO_RXFIFO0
  * @param id1 - 11 bit hex for standard id or 29 for extended
  * @param id2 - 11 bit hex for standard id or 29 for extended
  * @return uint32_t - error code defined in errno.h
  */
-sr_errno_t sr_fdcan_filter_add(FDCAN_HandleTypeDef* hfdcan, uint32_t filter_type, uint32_t filter_config, uint32_t id1, uint32_t id2);
+sr_errno_t sr_fdcan_filter_add(sr_fdcan_handle_t* handle, uint32_t filter_type, uint32_t filter_config, uint32_t id1, uint32_t id2);
 
 /**
- * @brief 
- * 
- * @param hfdcan can periph handle
+ * @brief
+ *
+ * @param handle can peripheral driver state
  * @param can_id id of can message
  * @param data pointer to byte array of data
  * @param length length of byte array
  * @return uint32_t - error code defined in errno.h
  */
-sr_errno_t sr_fdcan_tx(FDCAN_HandleTypeDef* hfdcan, uint32_t can_id, uint8_t* data, uint32_t length);
+sr_errno_t sr_fdcan_tx(sr_fdcan_handle_t* handle, uint32_t can_id, uint8_t* data, uint32_t length);
 
 /**
  * @brief Exact same as above but blocks until confirmed message has been sent
- * 
- * @param hfdcan 
- * @param can_id 
- * @param data 
- * @param length 
+ *
+ * @param handle
+ * @param can_id
+ * @param data
+ * @param length
  * @return sr_errno_t - error code defined in errno.h
  */
-sr_errno_t sr_fdcan_tx_blocking(FDCAN_HandleTypeDef* hfdcan, uint32_t can_id, uint8_t* data, uint32_t length);
+sr_errno_t sr_fdcan_tx_blocking(sr_fdcan_handle_t* handle, uint32_t can_id, uint8_t* data, uint32_t length);
 
 #endif
--- test_code/isotp_test/Core/Src/drivers/can_driver.c
+++ test_code/isotp_test/Core/Src/drivers/can_driver.c
@@ -3,12 +3,7 @@
 
 #include "drivers/can_driver.h"
 
-static uint32_t tx_id_type;
-static uint32_t tx_brs;
-static uint32_t tx_frame_format;
-static uint32_t tx_event_fifo_control;
-
-sr_errno_t sr_fdcan_config(FDCAN_HandleTypeDef* hfdcan, sr_fdcan_config_t* config_struct) {
+sr_errno_t sr_fdcan_config(sr_fdcan_handle_t* handle, FDCAN_HandleTypeDef* hfdcan, sr_fdcan_config_t* config_struct) {
     HAL_StatusTypeDef retval;
     // always always reject messages that don't pass through filter
     retval = HAL_FDCAN_ConfigGlobalFilter(hfdcan, FDCAN_REJECT, FDCAN_REJECT, FDCAN_REJECT_REMOTE, FDCAN_REJECT_REMOTE);
@@ -33,27 +28,28 @@ sr_errno_t sr_fdcan_config(FDCAN_HandleTypeDef* hfdcan, sr_fdcan_config_t* confi
         return SR_FDCAN_ACTIVATE_NOTIF_ERR;
     }
 
-    tx_id_type = config_struct->tx_id_type;
-    tx_brs = config_struct->tx_brs;
-    tx_frame_format = config_struct->tx_frame_format;
-    tx_event_fifo_control = config_struct->tx_event_fifo_control;
+    handle->hfdcan = hfdcan;
+    handle->tx_id_type = config_struct->tx_id_type;
+    handle->tx_brs = config_struct->tx_brs;
+    handle->tx_frame_format = config_struct->tx_frame_format;
+    handle->tx_event_fifo_control = config_struct->tx_event_fifo_control;
+    handle->filter_index = 0;
     return SR_OK;
 }
 
-sr_errno_t sr_fdcan_filter_add(FDCAN_HandleTypeDef* hfdcan, uint32_t filter_type, uint32_t filter_config, uint32_t id1, uint32_t id2) {
-    static uint32_t filter_index = 0;
-    uint32_t num_filters = hfdcan->Init.StdFiltersNbr;
+sr_errno_t sr_fdcan_filter_add(sr_fdcan_handle_t* handle, uint32_t filter_type, uint32_t filter_config, uint32_t id1, uint32_t id2) {
+    uint32_t num_filters = handle->hfdcan->Init.StdFiltersNbr;
 
-    if (filter_index < num_filters) {
+    if (handle->filter_index < num_filters) {
         FDCAN_FilterTypeDef filter = {
-            .IdType=tx_id_type,
-            .FilterIndex=filter_index,
+            .IdType=handle->tx_id_type,
+            .FilterIndex=handle->filter_index,
             .FilterType=filter_type,
             .FilterConfig=filter_config,
             .FilterID1=id1,
             .FilterID2=id2
         };
-        if (HAL_FDCAN_ConfigFilter(hfdcan, &filter) != HAL_OK) {
+        if (HAL_FDCAN_ConfigFilter(handle->hfdcan, &filter) != HAL_OK) {
             for (int i = 0; i < 10; i++) {
                 HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
                 HAL_Delay(500);
@@ -62,43 +58,43 @@ sr_errno_t sr_fdcan_filter_add(FDCAN_HandleTypeDef* hfdcan, uint32_t filter_type
             }
             return SR_FDCAN_CFG_FILTER_ERR;
         }
-        filter_index++;
+        handle->filter_index++;
         return SR_OK;
     } else {
         return SR_FDCAN_OVER_MAX_FILTERS;
     }
 }
 
-sr_errno_t sr_fdcan_tx(FDCAN_HandleTypeDef* hfdcan, uint32_t can_id, uint8_t* data, uint32_t length) {
+sr_errno_t sr_fdcan_tx(sr_fdcan_handle_t* handle, uint32_t can_id, uint8_t* data, uint32_t length) {
     if (length > FDCAN_DLC_BYTES_8) {
         return SR_FDCAN_TX_MSG_TOO_BIG;
     }
 
     FDCAN_TxHeaderTypeDef header = {
         .Identifier=can_id,
-        .IdType=tx_id_type,
+        .IdType=handle->tx_id_type,
         .TxFrameType=FDCAN_DATA_FRAME,
         .DataLength=length,
         .ErrorStateIndicator=FDCAN_ESI_ACTIVE,
-        .BitRateSwitch=tx_brs,
-        .FDFormat=tx_frame_format,
-        .TxEventFifoControl=tx_event_fifo_control,
+        .BitRateSwitch=handle->tx_brs,
+        .FDFormat=handle->tx_frame_format,
+        .TxEventFifoControl=handle->tx_event_fifo_control,
         .MessageMarker=0
     };
 
-    if (HAL_FDCAN_AddMessageToTxFifoQ(hfdcan, &header, data) != HAL_OK) {
+    if (HAL_FDCAN_AddMessageToTxFifoQ(handle->hfdcan, &header, data) != HAL_OK) {
         return SR_FDCAN_TX_ERR;
     }
     return SR_OK;
 }
 
-sr_errno_t sr_fdcan_tx_blocking(FDCAN_HandleTypeDef* hfdcan, uint32_t can_id, uint8_t* data, uint32_t length) {
-    sr_errno_t retval = sr_fdcan_tx(hfdcan, can_id, data, length);
+sr_errno_t sr_fdcan_tx_blocking(sr_fdcan_handle_t* handle, uint32_t can_id, uint8_t* data, uint32_t length) {
+    sr_errno_t retval = sr_fdcan_tx(handle, can_id, data, length);
     if (retval != SR_OK) {
         return retval;
     }
     // FIFO holds 3 messages max
-    while (HAL_FDCAN_GetTxFifoFreeLevel(hfdcan) < 3) {
+    while (HAL_FDCAN_GetTxFifoFreeLevel(handle->hfdcan) < 3) {
         // do nothing
     }
 
