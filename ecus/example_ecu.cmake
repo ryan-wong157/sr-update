be03b6a93e37ac3a9bcba2262e9e7bcf2ffcac81
Ryan Wong <wongrt0502@gmail.com>
2026-08-16 01:35:43 +1000

    feat: added example ecu cmake

--- ecus/example_ecu.cmake
+++ ecus/example_ecu.cmake
@@ -1,8 +1,8 @@
-# ECU definition: ecu_front
+# example_ecu config file
 # Which physical MCU this ECU builds against
 set(ECU_MCU stm32g431cb)
 
-# ECU identity + per-ECU CAN arbitration IDs, exposed to the firmware as compile definitions
+# ECU ID and expected isotp CAN IDS
 set(ECU_COMPILE_DEFS
     ECU_ID=0x10
     CFG_ISOTP_TX_ID=0x7A0
